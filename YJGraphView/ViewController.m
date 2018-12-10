f7b22a604f160fd2199f908c6b571d37c6162c19
Yangjing <yangjing@wordoor.com>
2018-12-10 16:28:55 +0800

    no message

--- YJGraphView/ViewController.m
+++ YJGraphView/ViewController.m
@@ -12,7 +12,7 @@
 #import "YJGraphBarView.h"
 #import "YJGraphPieView.h"
 
-@interface ViewController ()
+@interface ViewController () <UICollectionViewDelegate ,UICollectionViewDataSource>
 
 @end
 
@@ -33,8 +33,28 @@
     
     pieView.frame = CGRectMake(20, 20, CGRectGetWidth([UIScreen mainScreen].bounds)-40, CGRectGetWidth([UIScreen mainScreen].bounds)-40);
     
-    [pieView setDataArray:[@[@(0.02),  @(0.23),@(0.03), @(0.01), @(0.1), @(0.2), @(0.27), @(0.14)] mutableCopy]];
+    [pieView setDataArray:[@[@(0.02), @(0.23),@(0.03), @(0.01), @(0.1), @(0.2), @(0.27), @(0.14)] mutableCopy]];
 }
 
+//MARK: - collectionview datasource
+- (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section {
+    return 3;
+}
+
+- (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath {
+    return [collectionView dequeueReusableCellWithReuseIdentifier:@"cellId" forIndexPath:indexPath];
+}
+
+
+//MARK: - collectionview delegate
+- (void)collectionView:(UICollectionView *)collectionView willDisplayCell:(UICollectionViewCell *)cell forItemAtIndexPath:(NSIndexPath *)indexPath {
+    if (indexPath.row == 0) {
+        
+    } else if (indexPath.row == 1) {
+        
+    } else if (indexPath.row == 2) {
+        
+    }
+}
 
 @end
--- YJGraphView/YJGraphView/YJGraphPieView/YJGraphPieView.m
+++ YJGraphView/YJGraphView/YJGraphPieView/YJGraphPieView.m
@@ -17,13 +17,13 @@
 
     //将数组按从小到大顺序排序,顺带检测数据，饼状图数据总和不应该超过1
     for (NSInteger i = 0, count = dataArray.count-1; i < count; i++) {
-        CGFloat persent0 = [dataArray[i] floatValue];
-        totalPersent += persent0;
-        if (totalPersent > 1) {
-            NSLog(@"yangjing_%@: totalPersent > 1", NSStringFromClass([self class]));
-            return;
-        }
-        
+//        CGFloat persent0 = [dataArray[i] floatValue];
+//        totalPersent += persent0;
+//        if (totalPersent > 1) {
+//            NSLog(@"yangjing_%@: totalPersent > 1", NSStringFromClass([self class]));
+//            return;
+//        }
+//
         for (NSInteger j = 0, count = dataArray.count - 1 - i; j < count; j++) {
             CGFloat persent1 = [dataArray[j] floatValue];
             CGFloat persent2 = [dataArray[j+1] floatValue];
@@ -31,7 +31,11 @@
                 [dataArray exchangeObjectAtIndex:j withObjectAtIndex:j+1];
             }
         }
-
+    }
+    
+    //饼状图数据总和不足1的时候，补上不足的部位
+    if (totalPersent < 1) {
+//        [dataArray addObject:[NSNumber numberWithFloat:1-totalPersent]];
     }
     
     CGFloat viewWidth = CGRectGetWidth(self.bounds);
@@ -51,117 +55,42 @@
         CGFloat endAngle = currentAngle + 2*M_PI*persent;
         currentAngle = endAngle;
         
-        //画扇形
-        UIBezierPath *path = [UIBezierPath bezierPath];
-        [path moveToPoint:center];
-        [path addArcWithCenter:center radius:radius startAngle:startAngle endAngle:endAngle clockwise:YES];
-        
         //随机颜色
         UIColor *fillColor = [UIColor colorWithRed:(float)(arc4random()%100)/100 green:(float)(arc4random()%100)/100 blue:(float)(arc4random()%100)/100 alpha:1];
         
-        CAShapeLayer *shapeLayer = [CAShapeLayer new];
-        shapeLayer.strokeColor = [UIColor whiteColor].CGColor;
-        shapeLayer.fillColor = fillColor.CGColor;
-        shapeLayer.path = path.CGPath;
-        [self.layer addSublayer:shapeLayer];
+        //画扇形
+        UIBezierPath *sectorPath = [UIBezierPath bezierPath];
+        [sectorPath moveToPoint:center];
+        [sectorPath addArcWithCenter:center radius:radius startAngle:startAngle endAngle:endAngle clockwise:YES];
+
+        CAShapeLayer *sectorLayer = [CAShapeLayer new];
+        sectorLayer.strokeColor = [UIColor whiteColor].CGColor;
+        sectorLayer.fillColor = fillColor.CGColor;
+        sectorLayer.path = sectorPath.CGPath;
+        [self.layer addSublayer:sectorLayer];
         
         //标注
-        CGFloat centerAngle = (endAngle-startAngle)/2+startAngle;
+        UILabel *itemLabel = ({
+            UILabel *label = [[UILabel alloc] init];
+            label.font = [UIFont systemFontOfSize:10];
+            label.textColor = fillColor;
+            label.adjustsFontSizeToFitWidth = YES;
+            label.text = [NSString stringWithFormat:@"%.0f%%", persent*100];
+            label;
+        });
+        [self addSubview:itemLabel];
         
+        if (totalPersent < 1 && i == dataArray.count-1) {
+            itemLabel.text = [NSString stringWithFormat:@"Other %.0f%%", persent*100];
+        }
+        
+        //计算标注的位置，画标注线
+        //找出扇形圆弧的中点
+        CGFloat centerAngle = (endAngle-startAngle)/2+startAngle;
         CGFloat targetPointX = fabs(sqrt((radius*radius)/(1+tan(centerAngle)*tan(centerAngle))));
         CGFloat targetPointY = fabs(tan(centerAngle)*targetPointX);
         
-        //画标注线
-        UIBezierPath *path2 = [UIBezierPath bezierPath];
-        
-        CGFloat descHeight = [UIFont systemFontOfSize:10].lineHeight+10;
-        CGFloat linePointY;
-        
-        if (centerAngle > -M_PI/2.0 && centerAngle <= 0) {
-            targetPointX = center.x + (targetPointX + 5);
-            targetPointY = center.y - (targetPointY + 5);
-            
-            linePointY = descHeight*(i+1);
-            [path2 moveToPoint:CGPointMake(targetPointX, targetPointY)];
-            [path2 addLineToPoint:CGPointMake(viewWidth-40, linePointY)];
-            [path2 addLineToPoint:CGPointMake(viewWidth, linePointY)];
-            
-            UILabel *itemLabel = ({
-                UILabel *label = [[UILabel alloc] init];
-                label.font = [UIFont systemFontOfSize:10];
-                label.textColor = fillColor;
-                label.textAlignment = NSTextAlignmentRight;
-                label.adjustsFontSizeToFitWidth = YES;
-                label.text = [NSString stringWithFormat:@"%.0f%%", persent*100];
-                label;
-            });
-            [self addSubview:itemLabel];
-            itemLabel.frame = CGRectMake(viewWidth-60, linePointY-[UIFont systemFontOfSize:6].lineHeight-3, 60, [UIFont systemFontOfSize:6].lineHeight);
-
-        } else if (centerAngle > 0 && centerAngle <= M_PI/2.0) {
-            targetPointX = center.x + (targetPointX + 5);
-            targetPointY = center.y + (targetPointY + 5);
-            
-            linePointY = descHeight*(i+1)+center.y;
-            [path2 moveToPoint:CGPointMake(targetPointX, targetPointY)];
-            [path2 addLineToPoint:CGPointMake(viewWidth-40, linePointY)];
-            [path2 addLineToPoint:CGPointMake(viewWidth, linePointY)];
-            
-            UILabel *itemLabel = ({
-                UILabel *label = [[UILabel alloc] init];
-                label.font = [UIFont systemFontOfSize:10];
-                label.textColor = fillColor;
-                label.textAlignment = NSTextAlignmentRight;
-                label.adjustsFontSizeToFitWidth = YES;
-                label.text = [NSString stringWithFormat:@"%.0f%%", persent*100];
-                label;
-            });
-            [self addSubview:itemLabel];
-            itemLabel.frame = CGRectMake(viewWidth-60, linePointY-[UIFont systemFontOfSize:6].lineHeight-3, 60, [UIFont systemFontOfSize:6].lineHeight);
-            
-        } else if (centerAngle > M_PI/2.0 && centerAngle <= M_PI) {
-            targetPointX = center.x - (targetPointX + 5);
-            targetPointY = center.y + (targetPointY + 5);
-            
-            linePointY = descHeight*(i+1)+center.y;
-            [path2 moveToPoint:CGPointMake(targetPointX, targetPointY)];
-            [path2 addLineToPoint:CGPointMake(40, linePointY)];
-            [path2 addLineToPoint:CGPointMake(0, linePointY)];
-            
-            UILabel *itemLabel = ({
-                UILabel *label = [[UILabel alloc] init];
-                label.font = [UIFont systemFontOfSize:10];
-                label.textColor = fillColor;
-                label.textAlignment = NSTextAlignmentLeft;
-                label.adjustsFontSizeToFitWidth = YES;
-                label.text = [NSString stringWithFormat:@"%.0f%%", persent*100];
-                label;
-            });
-            [self addSubview:itemLabel];
-            itemLabel.frame = CGRectMake(0, linePointY-[UIFont systemFontOfSize:6].lineHeight-3, 60, [UIFont systemFontOfSize:6].lineHeight);
-            
-        } else {
-            targetPointX = center.x - (targetPointX + 5);
-            targetPointY = center.y - (targetPointY + 5);
-            
-            linePointY = descHeight*(i+1);
-            [path2 moveToPoint:CGPointMake(targetPointX, targetPointY)];
-            [path2 addLineToPoint:CGPointMake(40, linePointY)];
-            [path2 addLineToPoint:CGPointMake(0, linePointY)];
-            
-            UILabel *itemLabel = ({
-                UILabel *label = [[UILabel alloc] init];
-                label.font = [UIFont systemFontOfSize:10];
-                label.textColor = fillColor;
-                label.textAlignment = NSTextAlignmentLeft;
-                label.adjustsFontSizeToFitWidth = YES;
-                label.text = [NSString stringWithFormat:@"%.0f%%", persent*100];
-                label;
-            });
-            [self addSubview:itemLabel];
-            itemLabel.frame = CGRectMake(0, linePointY-[UIFont systemFontOfSize:6].lineHeight-3, 60, [UIFont systemFontOfSize:6].lineHeight);
-        }
-        
+        //标注线的起始点
         UIView *pointView = ({
             UIView *view = [[UIView alloc] init];
             view.backgroundColor = fillColor;
@@ -170,30 +99,75 @@
         });
         [self addSubview:pointView];
         pointView.frame = CGRectMake(0, 0, 4, 4);
-        pointView.center = CGPointMake(targetPointX, targetPointY);
+        
+        //画标注线
+        UIBezierPath *linePath = [UIBezierPath bezierPath];
+        
+        CGFloat descHeight = [UIFont systemFontOfSize:10].lineHeight+10;
+        
+        //ps:数学不太好，这里很麻烦的把坐标系分成四个象限区分处理，应该能有更好的方法
+        if (centerAngle > -M_PI/2.0 && centerAngle <= 0) {
+            //标注线的起始点设在对应扇形圆弧中点离圆弧5px的位置
+            targetPointX = center.x + (targetPointX + 5);
+            targetPointY = center.y - (targetPointY + 5);
+            pointView.center = CGPointMake(targetPointX, targetPointY);
+
+            //画标注线，标注线为一条从起始点到
+            CGFloat linePointY = descHeight*(i+1);
+            [linePath moveToPoint:CGPointMake(targetPointX, targetPointY)];
+            [linePath addLineToPoint:CGPointMake(viewWidth-40, linePointY)];
+            [linePath addLineToPoint:CGPointMake(viewWidth, linePointY)];
+
+            itemLabel.textAlignment = NSTextAlignmentRight;
+            itemLabel.frame = CGRectMake(viewWidth-60, linePointY-[UIFont systemFontOfSize:6].lineHeight-3, 60, [UIFont systemFontOfSize:6].lineHeight);
+
+        } else if (centerAngle > 0 && centerAngle <= M_PI/2.0) {
+            targetPointX = center.x + (targetPointX + 5);
+            targetPointY = center.y + (targetPointY + 5);
+            pointView.center = CGPointMake(targetPointX, targetPointY);
+
+            CGFloat linePointY = descHeight*(i+1)+center.y;
+            [linePath moveToPoint:CGPointMake(targetPointX, targetPointY)];
+            [linePath addLineToPoint:CGPointMake(viewWidth-40, linePointY)];
+            [linePath addLineToPoint:CGPointMake(viewWidth, linePointY)];
+            
+            itemLabel.textAlignment = NSTextAlignmentRight;
+            itemLabel.frame = CGRectMake(viewWidth-60, linePointY-[UIFont systemFontOfSize:6].lineHeight-3, 60, [UIFont systemFontOfSize:6].lineHeight);
+            
+        } else if (centerAngle > M_PI/2.0 && centerAngle <= M_PI) {
+            targetPointX = center.x - (targetPointX + 5);
+            targetPointY = center.y + (targetPointY + 5);
+            pointView.center = CGPointMake(targetPointX, targetPointY);
+
+            CGFloat linePointY = descHeight*(i+1)+center.y;
+            [linePath moveToPoint:CGPointMake(targetPointX, targetPointY)];
+            [linePath addLineToPoint:CGPointMake(40, linePointY)];
+            [linePath addLineToPoint:CGPointMake(0, linePointY)];
+            
+            itemLabel.textAlignment = NSTextAlignmentLeft;
+            itemLabel.frame = CGRectMake(0, linePointY-[UIFont systemFontOfSize:6].lineHeight-3, 60, [UIFont systemFontOfSize:6].lineHeight);
+            
+        } else {
+            targetPointX = center.x - (targetPointX + 5);
+            targetPointY = center.y - (targetPointY + 5);
+            pointView.center = CGPointMake(targetPointX, targetPointY);
+
+            CGFloat linePointY = descHeight*(i+1);
+            [linePath moveToPoint:CGPointMake(targetPointX, targetPointY)];
+            [linePath addLineToPoint:CGPointMake(40, linePointY)];
+            [linePath addLineToPoint:CGPointMake(0, linePointY)];
+            
+            itemLabel.textAlignment = NSTextAlignmentLeft;
+            itemLabel.frame = CGRectMake(0, linePointY-[UIFont systemFontOfSize:6].lineHeight-3, 60, [UIFont systemFontOfSize:6].lineHeight);
+        }
         
         CAShapeLayer *shapeLayer2 = [CAShapeLayer new];
         shapeLayer2.strokeColor = fillColor.CGColor;
         shapeLayer2.fillColor = [UIColor clearColor].CGColor;
-        shapeLayer2.path = path2.CGPath;
+        shapeLayer2.path = linePath.CGPath;
         shapeLayer2.lineWidth = 1;
         [self.layer addSublayer:shapeLayer2];
         
-    }
-    
-    if (currentAngle < 2*M_PI-M_PI/2.0) {
-        
-        UIBezierPath *path = [UIBezierPath bezierPath];
-        [path moveToPoint:CGPointMake(viewWidth/2, viewWidth/2)];
-        [path addArcWithCenter:CGPointMake(viewWidth/2, viewWidth/2) radius:radius startAngle:currentAngle endAngle:2*M_PI-M_PI/2.0 clockwise:YES];
-        
-        //画圆
-        UIColor *fillColor = [UIColor colorWithRed:(float)(arc4random()%100)/100 green:(float)(arc4random()%100)/100 blue:(float)(arc4random()%100)/100 alpha:1];
-        CAShapeLayer *shapeLayer = [CAShapeLayer new];
-        shapeLayer.strokeColor = [UIColor whiteColor].CGColor;
-        shapeLayer.fillColor = fillColor.CGColor;
-        shapeLayer.path = path.CGPath;
-        [self.layer addSublayer:shapeLayer];
     }
     
     UIBezierPath *path = [UIBezierPath bezierPathWithArcCenter:center radius:viewWidth/2 startAngle:-M_PI/2.0 endAngle:2*M_PI-M_PI/2.0 clockwise:YES];
